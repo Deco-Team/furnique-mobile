c107dd0b82409c132c94672088bf11dd315a2b50
sarahnguyen <thanhhao2832@gmail.com>
2024-06-04 17:06:26 +0700

    Fix build fail

--- app/src/main/java/com/example/furnique/activities/Home.java
+++ app/src/main/java/com/example/furnique/activities/Home.java
@@ -56,12 +56,5 @@ public class Home extends AppCompatActivity {
             }
         });
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-        binding = ActivityHomeBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
     }
 }
