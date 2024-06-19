99c702a9ca5e0e61c777cfe08551c1b7701710cd
PhongNH <andyhoaiphong2003@gmail.com>
2024-06-19 09:07:52 +0700

    [Implement][SignUp & Checkout] Implement Checkout XML and signup activity

--- app/src/main/java/com/example/furnique/models/SignUpModel.java
+++ app/src/main/java/com/example/furnique/models/SignUpModel.java
@@ -2,23 +2,27 @@ package com.example.furnique.models;
 
 import android.util.Log;
 
+import com.example.furnique.activities.SignUpActivity;
 import com.example.furnique.contracts.Constants;
 import com.example.furnique.dto.request.SignUpDTO;
 import com.example.furnique.dto.response.SuccessResponseDTO;
 import com.example.furnique.models.interfaces.SignUpAPI;
+
+import java.util.Objects;
 
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
-
 public class SignUpModel {
 
     private final SignUpDTO signUpDTO;
+    private final SignUpActivity signUpActivity;
 
-    public SignUpModel(SignUpDTO signUpDTO) {
+    public SignUpModel(SignUpDTO signUpDTO, SignUpActivity signUpActivity) {
         this.signUpDTO = signUpDTO;
+        this.signUpActivity = signUpActivity;
     }
 
     public void fetchSignUp() {
@@ -30,14 +34,17 @@ public class SignUpModel {
         signUpAPI.registerCustomer(signUpDTO).enqueue(new Callback<SuccessResponseDTO>() {
             @Override
             public void onResponse(Call<SuccessResponseDTO> call, Response<SuccessResponseDTO> response) {
-                Log.d("Response","success");
+                if(Objects.equals(response.message(), "Bad Request")){
+                    signUpActivity.onSignUpFailure();
+                } else {
+                    signUpActivity.onSignUpSuccess();
+                }
             }
 
             @Override
             public void onFailure(Call<SuccessResponseDTO> call, Throwable t) {
-                System.out.println(t);
+                Log.d("Response","t");
             }
         });
     }
-
 }
